2ce3575270df12429b72b261d48560460f304828
naoki matsuyama <naokiwpc@gmail.com>
2026-09-20 14:22:31 +0900

    v0.4 ドラを独立した節に(どこから来るか・次の牌の例4つ・赤ドラ・裏ドラ)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-/* 麻雀ドリル v0.3 */
-const CACHE = 'mahjong-v0.3';
+/* 麻雀ドリル v0.4 */
+const CACHE = 'mahjong-v0.4';
 const FILES = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
