48bea60e08b06c8f6a0e797c96ada35fd4a69d87
naoki matsuyama <naokiwpc@gmail.com>
2026-09-20 13:52:07 +0900

    v0.2 入口を追加(はじめに・かたまり表示・用語の説明)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-/* 麻雀ドリル v0.1 */
-const CACHE = 'mahjong-v0.1';
+/* 麻雀ドリル v0.2 */
+const CACHE = 'mahjong-v0.2';
 const FILES = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
