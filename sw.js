dbb23bdb0b591721c620f31d934829084bb7a204
naoki matsuyama <naokiwpc@gmail.com>
2026-09-20 18:15:44 +0900

    v0.8 答えが問題に書いてある段を廃止。第1章を作り直し(読み→牌・么九牌)。ステージ検査を新設

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-/* 麻雀ドリル v0.7 */
-const CACHE = 'mahjong-v0.7';
+/* 麻雀ドリル v0.8 */
+const CACHE = 'mahjong-v0.8';
 const FILES = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
